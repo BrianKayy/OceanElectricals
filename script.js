a427d9ce29572c8a2e5ad60af7a782239184e4ae
BrianKayy <Briankasumba40@gmail.com>
2023-03-24 10:35:14 +0400

    search bar has been added

---
+++ script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $(".input").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $(".main  .shop-container").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
